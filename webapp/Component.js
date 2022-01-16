132670c84d2063c676735c5a1cf8667cc57ae15d
GregoryPauwelsG <97838451+GregoryPauwelsG@users.noreply.github.com>
2022-01-16 20:00:43 +0100

    First change
    Wrong Import parameter Component

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,4 +1,4 @@
-sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent", "sap/f/LayoutType"], function(AppComponent, LayoutType) {
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function(AppComponent) {
     return AppComponent.extend("zsd031hoteltool.Component", {
         metadata: {
             manifest: "json"
